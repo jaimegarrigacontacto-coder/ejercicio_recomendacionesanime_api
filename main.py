7f434390466dd779542528c8cfdee9a00d8e885f
Sara Beldjella <sarabeldjella98@gmail.com>
2025-10-29 19:44:05 +0100

    Configurar menú principal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from connection import *
 
 opcion = None
+login = None
 
 host = "localhost"
 root = input("Introduce el usuario: ")
@@ -8,8 +9,22 @@ password = input("Introduce la contraseña: ")
 
 myDB = Connection(host, root, password)
 
+while login != 0:
+    print("\n LOGIN: RECOMENDACIONES ANIME\n--------------\n1. Iniciar sesión\n2. Registrarse\n0. Salir")
+    login = input("\nSelecciona opción: ")
+    if login.isdigit():
+        login = int(login)
+    else:
+        print("Error. Opción no válida.")
+        continue
+
+    if login == 0:
+        print("Saliendo del programa...")
+        opcion = 0
+
+
 while opcion != 0:
-    print("\n1- Añadir anime\n2- Recomendaciones")
+    print("\n1- Añadir valoración anime\n2- Recomendaciones")
     opcion = input("\nSelecciona opción: ")
     if opcion.isdigit():
         opcion = int(opcion)
